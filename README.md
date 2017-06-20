fc4568404da50083fbf41b9d8751ee227db39a17
Barbara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-06-20 16:34:49 +0100

    Adds function of reading notes; cleaning up the code

---
+++ README.md
@@ -0,0 +1,7 @@
+## Commnad line Node.js notes app
+
+- clone this repo
+- run `npm install`
+- to add a not run with your title and body of the note `node app.js add --title=yourtitle ---body=yournote`
+
+- There are other commands like remove, list, and read 
--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,26 +6,30 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv);
 
 if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
   if (note) {
     console.log('Note created');
-    console.log('--');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    notes.logNote(note);
   } else {
     console.log('Note title taken');
   }
 } else if (command === 'list') {
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('Note found');
+    notes.logNote(note);
+  } else {
+    console.log('Note not found');
+  }
 } else if (command === 'remove') {
   var noteRemoved = notes.removeNote(argv.title);
-  var message = noteRemoved ? "Note was removed" : "No such title exists"
+  var message = noteRemoved ? 'Note was removed' : 'Note not found';
   console.log(message);
 } else {
   console.log('Command not recognized');
